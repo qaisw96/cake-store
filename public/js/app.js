9bca0a05eaa23934a157ea5fdc21059918bac3ef
qaisw96 <qaisw14@gmail.com>
2021-04-24 15:32:31 +0300

    ddd

--- public/js/app.js
+++ public/js/app.js
@@ -81,9 +81,25 @@ $('.delete-form').on('submit',function authenticate(event) {
                     // alert("successfully authenticated!")
                 } else if(data.status == "failed") {
                     // alert("failed authentication!")
-                }
-                
             }
-        });
-        $(this).fadeIn(300)
-        })
+                
+    }
+  });
+ $(this).fadeIn(300)
+})
+
+
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+    $('.nav-css').attr('class', 'nav-fix')
+    if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+    } else {
+    header.classList.remove("sticky");
+    }
+}
+        
